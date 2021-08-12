e0bf83de827140638c309fbb52a8eae3538d976e
Ana Beatriz Morita <anabemorita@gmail.com>
2021-08-12 20:27:01 -0300

    Registrar ExitTicket no admin

--- exit_ticket/admin.py
+++ exit_ticket/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from exit_ticket.models import ExitTicket
 
 # Register your models here.
+
+class ExitTicketAdmin(admin.ModelAdmin):
+    list_filter = ['nome']
+    list_display = ['nome', 'data', 'nota']
+
+admin.site.register(ExitTicket, ExitTicketAdmin)
